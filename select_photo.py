ce569eea6b6f57c849fa117f4341b62139815e8a
Lynnae Bryan <lynnaeg@pdx.edu>
2019-02-28 11:45:23 -0800

    added sort_bins

--- select_photo.py
+++ select_photo.py
@@ -7,10 +7,16 @@ from photos import preprocessor
 MAX_TAGS = 51
 
 
+def sort_bins():
+  bins = [Bin(i+1) for i in range(MAX_TAGS)]
+  # assign photos to bins
+  photo_set = preprocessor('b_lovely_landscapes.txt')
 
-bins = [Bin(i+1) for i in range(MAX_TAGS)]
-# assign photos to bins
-
+  while photo_set:
+    curr_p = photo_set.pop()
+    idx = len(curr_p.tags)
+    bins[idx -1] = bins[idx-1].add(curr_p)
+  return bins
 
 
 
