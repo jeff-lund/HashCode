cfc390713bec2acb9093ac633332ae528105e3f4
Lynnae Bryan <lynnaeg@pdx.edu>
2019-02-28 11:00:31 -0800

    selection algorithm notes

--- select_photo.py
+++ select_photo.py
@@ -1,15 +1,21 @@
-# select first image from highest bin
-# --> pop at random, or whatevers
-# for the first image, intersect with next image first, and then again, making a two-branch tree
-# --> use a list, add to the front and back alternately
-# for next image, intersect with front/back image, from bin 
 
-# current bin
-
-# start from same seed image
+# start both lists from same seed image
 slideshow_front = []
 slideshow_back = []
 
-# choose first image
-# choose randomly from highest n bin
-#
+# choose first image, P_1
+# --> choose randomly from highest bin, b
+# set current_bin to b
+
+# choose next image, n_i
+# alternate between two slideshow lists:
+# walk over b, try to find image that best intersects with P_1
+# --> Best intersection is n/2 number of tags, allow for solution with n/2 - 1 (can change to n/2 -2 if needed)
+# if best intersection not in current_bin, go to next highest bin
+# repeat until best solution found
+# --> if not found, end list
+# append n_i to slideshow list
+
+# Change current bin, b to next highest bin once b is empty.
+
+
