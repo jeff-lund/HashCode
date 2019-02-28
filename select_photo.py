1e651e1fcdd4ac17a2727163d6529957c2002000
Lynnae Bryan <lynnaeg@pdx.edu>
2019-02-28 10:53:43 -0800

    adding select_photo.py file

---
+++ select_photo.py
@@ -0,0 +1,15 @@
+# select first image from highest bin
+# --> pop at random, or whatevers
+# for the first image, intersect with next image first, and then again, making a two-branch tree
+# --> use a list, add to the front and back alternately
+# for next image, intersect with front/back image, from bin 
+
+# current bin
+
+# start from same seed image
+slideshow_front = []
+slideshow_back = []
+
+# choose first image
+# choose randomly from highest n bin
+#
