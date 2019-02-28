f58910b1efd1f0efb1be9e31146155fdf8733fe9
Lynnae Bryan <lynnaeg@pdx.edu>
2019-02-28 12:33:55 -0800

    reverse bug fixed

--- sort_bins.py
+++ sort_bins.py
@@ -5,7 +5,8 @@ from photos import preprocessor
 
 def sort_bins(photo_set, max_tags):
   """ Returns bins, sorted in descending order. Note bins are labeled at index +1 (index 0 count, bins -> index +1) """
-  bins = [Bin(i+1) for i in range(max_tags)].reverse()
+  bins = [Bin(i+1) for i in range(max_tags)]
+  bins.reverse()
   # assign photos to bins
   while photo_set:
     curr_p = photo_set.pop()
