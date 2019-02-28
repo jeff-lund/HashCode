33f3cfb3cf7d3c44c2fc8c7f4aa37331aa72feaa
jeff-lund <jdl3@pdx.edu>
2019-02-28 12:54:26 -0800

    Jeff fucked up:

---
+++ select_this.py
@@ -0,0 +1,28 @@
+def select_(bins):
+  # start both lists from same seed image
+  slideshow_front = []
+  slideshow_back = []
+  # choose first image, P_1
+  # --> choose randomly from highest bin, b
+  # set current_bin to b
+  current_bin = 0
+  while not bins[current_bin].photos:
+      current_bin += 1
+
+  current_image = bins[current_bin].photos.pop()
+  slideshow_front.append(current_image)
+  slideshow_back.append(current_image)
+
+  while bins[current_bin].photos:
+    flag = False
+    threshold = bins[current_bin].num_tags // 2 #fix me?
+    for image in bins[current_bin].photos:
+      if (current_image.tags & image.tags) >= threshold:
+        slideshow_front.append(image)
+        current_image = image
+        bins[current_bin].photos.remove(image)
+        flag = True
+
+    if not bins[current_bin].photos:
+      current_bin += 1
+  return slideshow_front
